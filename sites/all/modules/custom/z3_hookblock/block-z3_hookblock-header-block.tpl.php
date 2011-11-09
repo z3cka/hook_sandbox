7ef7d21e99dd1913ac4e1c2da53e3e92b5204719
Casey Grzecka <c@sey.gr>
2011-11-09 13:15:28 -0800

    module now does what it says :-)

---
+++ sites/all/modules/custom/z3_hookblock/block-z3_hookblock-header-block.tpl.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file block-z3_hookblock-header-block.tpl.php
+ *
+ * Theme implementation to display a custom block.
+ *
+ * Available variables:
+ * - $section: this block's content
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_block()
+ */
+dsm(get_defined_vars());
+?>
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">
+<?php if ($block->subject): ?>
+  <h2><?php print $block->subject ?></h2>
+<?php endif;?>
+	<h1>I'm printed from a template from within a module!</h1>
+  <div class="content">
+    <?php print $block->content ?>
+		<?php print $section ?>
+  </div>
+</div>
